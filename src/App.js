da1edf49ef42f510d0ecfb4706023ccb7902417f
rajesh <merajesh04@gmail.com>
2019-09-18 23:10:34 +0530

    Make api call to unsplashed for random image url

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h3>Word It</h3> 
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageUrl: ''
+    };
+
+  }
+
+  componentDidMount() {
+    // set up the config for the api auth
+    const config = {
+      headers: {
+        "Authorization": "Client-ID " + process.env.REACT_APP_UNSPLASH_API_KEY
+      }
+    };
+
+    // make the api call to the unsplash server
+    Axios.get(
+      'https://api.unsplash.com/photos/random/',
+      config
+    ).then(res => {
+      console.log(res)
+
+      console.log(res.data.urls.regular);
+
+      // set the image url state
+      this.setState({
+        imageUrl: res.data.urls.regular
+      });
+    }).catch(err => {
+      console.log(err);
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <h3>Word It</h3> 
+
+       <img src={this.state.imageUrl} alt='random image' width="300px"/>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
