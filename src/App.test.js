e7af591c5c4d72d9db061e3c244ed553938e591b
Horsicode <horsicode@gmail.com>
2021-10-17 08:04:28 +0000

    add test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+// @ponicode
+describe("componentDidMount", () => {
+    let object
+    let inst
+
+    beforeEach(() => {
+        object = [["Pierre Edouard", "Pierre Edouard", "Jean-Philippe"], ["Anas", "Michael", "Pierre Edouard"], ["Jean-Philippe", "Pierre Edouard", "George"]]
+        inst = new App.default(object)
+    })
+
+    test("0", () => {
+        let callFunction = () => {
+            inst.componentDidMount()
+        }
+    
+        expect(callFunction).not.toThrow()
+    })
+})
